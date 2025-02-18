bdae86feec427ee6b420590f01c6e8b3a8c6b95d
Cayke Veras <caykedanielcd@gmail.com>
2025-02-18 03:46:27 -0300

    dik

--- src/app/components/ui/SoundButton.tsx
+++ src/app/components/ui/SoundButton.tsx
@@ -35,13 +35,20 @@ export function SoundButton({
     <div className="relative flex flex-col items-center group" title={title}>
       <button
         className={clsx(
-          "w-[200px] h-[200px] drop-shadow-md rounded-md bg-slate-700 text-white text-2xl font-bold flex items-center justify-center hover:scale-105 transition-all",
+          "w-[200px] h-[200px] flex-col gap-2 drop-shadow-md rounded-md bg-slate-700 text-white text-2xl font-bold flex items-center justify-center group-hover:scale-105 transition-all",
           active ? "opacity-100" : "opacity-65",
           className
         )}
         onClick={onClick}
       >
         {icon}
+        <p className="text-base">{title.substring(2)}</p>
+        {active && (
+          <div
+            className={`absolute left-0 min-h-full bg-slate-800/85 bg-accent-slate-400 rounded-md -z-10`}
+            style={{ width: `${volume * 100}%` }}
+          />
+        )}
       </button>
 
       {active && showVolume && (
@@ -58,8 +65,8 @@ export function SoundButton({
           step="0.01"
           value={volume}
           onChange={(e) => onVolumeChange(parseFloat(e.target.value))}
-          className="absolute bottom-[0px] w-[80%] opacity-0 group-hover:opacity-100
-          bg-slate-800 accent-slate-400 rounded-md appearance-none cursor-pointer
+          className="absolute bottom-0 w-[80%] opacity-0 group-hover:opacity-100
+          bg-slate-800/85 accent-slate-400 rounded-md appearance-none cursor-pointer
            transition-all"
         />
       )}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,26 +6,40 @@ import {
   FlameKindling,
   PlayCircle,
   StopCircle,
+  Waves,
+  Zap,
 } from "lucide-react";
 import { motion } from "framer-motion";
 import { AudioButton } from "./components/AudioButton";
 
+const iconSize = 64;
+
 const sounds = [
   {
     src: "/sound/rain.mp3",
-    icon: <Cloud size={64} />,
+    icon: <Cloud size={iconSize} />,
     title: "🌧️Chuva",
   },
   {
     src: "/sound/fire.mp3",
-    icon: <FlameKindling size={64} />,
+    icon: <FlameKindling size={iconSize} />,
     title: "🔥Fogo",
   },
   {
     src: "/sound/bird.mp3",
-    icon: <Bird size={64} />,
+    icon: <Bird size={iconSize} />,
     title: "🐦Pássaros",
   },
+  {
+    src: "/sound/wind.mp3",
+    icon: <Waves size={iconSize} />,
+    title: "🌬️Vento",
+  },
+  {
+    src: "/sound/thunder.mp3",
+    icon: <Zap size={iconSize} />,
+    title: "⚡ Trovões",
+  }
 ];
 export default function Home() {
   const { soundsActive, toggleActive } = useActiveContext();
@@ -35,7 +49,10 @@ export default function Home() {
   };
 
   return (
-    <div className="relative w-[800px] h-[400px] bg-slate-900 grid grid-cols-3 items-center justify-center mx-auto my-auto">
+    <div
+      className="relative w-[800px] py-12 bg-slate-900
+     grid grid-cols-3 gap-y-4 items-center justify-center mx-auto my-auto"
+    >
       <motion.button
         className="
          absolute bottom-[-16px] right-0 left-0 px-4 py-2 
